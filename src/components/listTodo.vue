0653007e51c7974d8087f2cdc604c33815000294
Jois <putujois0703@gmail.com>
2024-11-12 11:41:20 +0800

    perbaikan tampilan navbar dan todo list

--- src/components/listTodo.vue
+++ src/components/listTodo.vue
@@ -1,15 +1,36 @@
 <template>
   <section class="todo">
-    <div class="m-3 mt-8 text-xl">Todo List</div>
-    <form action="#">
-      <!-- kategori -->
-      <!-- <input
-        class="kategori border border-black rounded-md py-2 px-8 m-3"
-        type="text"
-        v-model="kategori"
-        placeholder="masukkan kategori, misal:PR"
-      /> -->
-      <!-- todolistnya apa -->
+    <div class="card bg-white shadow-xl m-3 border rounded-md p-5">
+    <div class="m-3  text-xl">Todo List</div>
+      <div
+        :class="['todolist flex justify-between border', `border-${status}`, 'rounded-md m-3']"
+        v-for="(todo, index) in todoList"
+        :key="index"
+      >
+        <div class="content flex m-3 gap-8">
+          <input class="checkbox" type="checkbox" v-model="doneTodo" />
+          <div class="todo__content">{{ todo.todo }}</div>
+        </div>
+        <!-- status -->
+        <!-- Tombol selesai && tombol pending -->
+        <!-- jika tombol selesai di klik, maka border akan berwarna hijau -->
+        <!-- jika tombol pending di klik, maka border akan berwarna kuning -->
+        <div class="status">
+          <button
+            class="pending py-2 px-2 m-2 bg-yellow-300 rounded-md hover:bg-yellow-400"
+            @click.prevent="changeStatus"
+          >
+            Pending
+          </button>
+          <button
+            class="hapus py-2 px-2 m-2 bg-red-500 hover:bg-red-600 rounded-md"
+            @click.prevent="deleteTodo"
+          >
+            Hapus
+          </button>
+        </div>
+      </div>
+      <form action="#">
       <input
         class="todo border border-black rounded-md py-2 px-8 m-3"
         type="text"
@@ -18,33 +39,6 @@
       />
       <button @click.prevent="addTodo" class="py-2 px-2 m-2 bg-blue-500 rounded-md">Enter</button>
     </form>
-    <div class="card bg-white shadow-xl m-3 border rounded-md p-5">
-      <div
-        :class="['todolist flex justify-between border', `border-${status}`, 'rounded-md m-3']"
-        v-for="(todo, index) in todoList"
-        :key="index"
-      >
-        <div class="content flex m-3 gap-8">
-          <input class="checkbox" type="checkbox" @click.prevent="doneTodo" />
-          <!-- <div class="todo__kategori">{{ todo.kategori }}</div> -->
-          <div class="todo__content">{{ todo.todo }}</div>
-        </div>
-        <!-- status -->
-        <!-- Tombol selesai && tombol pending -->
-        <!-- jika tombol selesai di klik, maka akan bepindah ke done -->
-        <!-- jika tombol pending di klik, maka akan bepindah ke pending -->
-        <div class="status">
-          <button
-            class="pending py-2 px-2 m-2 bg-yellow-300 rounded-md"
-            @click.prevent="changeStatus"
-          >
-            Pending
-          </button>
-          <button class="hapus py-2 px-2 m-2 bg-red-500 rounded-md" @click.prevent="deleteTodo">
-            Hapus
-          </button>
-        </div>
-      </div>
     </div>
   </section>
 </template>
@@ -69,13 +63,13 @@ export default {
           status: 'pendding',
         },
       ],
+  
     }
   },
   methods: {
     addTodo() {
       if (this.newTodo.trim() !== '') {
         this.todoList.push({
-          kategori: this.kategori,
           todo: this.newTodo,
         })
         this.newTodo = ''
@@ -102,8 +96,12 @@ export default {
         this.status = status
       }
     },
-    doneTodo(index) {
-      this.todoList[index].status = 'done'
+    doneTodo() {
+      let status = 'gray-600'
+      if (this.status == 'gray-600') {
+        status = 'green-500'
+      }
+      this.status = status
     },
   },
 }
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -2,8 +2,11 @@
 import Logoipsum from './icons/logoipsum.png'
 </script>
 <template>
-  <nav class="navbar flex justify-between bg-white shadow-xl p-8">
-    <img class="w-28" src="./icons/logoipsum.png" alt="" />
-    <div to="#">Home</div>
+  <nav class="navbar flex justify-between bg-white shadow-xl p-6">
+    <img class="w-48" src="./icons/logoipsum.png" alt="" />
+    <div class="profile flex items-center gap-4 text-xl mx-5">
+        <img class="w-8  rounded-full" src="./icons/image.jpg" alt="" />
+        <div >Jois Tika</div>
+    </div>
   </nav>
 </template>
