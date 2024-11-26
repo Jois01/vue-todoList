71e0e64d4e2eef591b2ac1fb5f7c34a094908631
Jois <putujois0703@gmail.com>
2024-11-26 10:35:06 +0800

    perbaikan desain di navbar dan tidi list

--- src/components/listTodo.vue
+++ src/components/listTodo.vue
@@ -26,19 +26,45 @@
         <!-- jika tombol pending di klik, maka border akan berwarna kuning -->
         <div class="status">
           <button
-            class="pending py-2 px-2 m-2 bg-yellow-300 rounded-md hover:bg-yellow-400"
+            class="pending md:py-2 md:px-2 p-1 m-2 bg-yellow-300 rounded-md hover:bg-yellow-400"
             @click.prevent="todoPending(todo)"
             :class="{
               hidden: todo.isDone,
             }"
           >
-            Pending
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-4 md:size-6"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+              />
+            </svg>
           </button>
           <button
-            class="hapus py-2 px-2 m-2 bg-red-500 hover:bg-red-600 rounded-md"
+            class="hapus md:py-2 md:px-2 p-1 m-2 bg-red-500 rounded-md hover:bg-red-600"
             @click.prevent="deleteTodo(todo)"
           >
-            Hapus
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke-width="1.5"
+              stroke="currentColor"
+              class="size-4 md:size-6 stroke-white"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+              />
+            </svg>
           </button>
         </div>
       </div>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -3,10 +3,10 @@ import Logoipsum from './icons/logoipsum.png'
 </script>
 <template>
   <nav class="navbar flex justify-between bg-white shadow-xl p-6">
-    <img class="w-48" src="./icons/logoipsum.png" alt="" />
+    <img class="w-28 md:w-48" src="./icons/logoipsum.png" alt="" />
     <div class="profile flex items-center gap-4 text-xl mx-5">
-        <img class="w-8  rounded-full" src="./icons/image.jpg" alt="" />
-        <div >Jois Tika</div>
+      <img class="w-8 rounded-full" src="./icons/image.jpg" alt="" />
+      <div class="text-sm md:text-xl">Jois Tika</div>
     </div>
   </nav>
 </template>
